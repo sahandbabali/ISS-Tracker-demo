826f7a1893c87c2a810527cbfa6efde963d6144b
sahandbabali <sahand.babali@gmail.com>
2021-07-04 04:13:48 +0430

    color changes + path

--- app.js
+++ app.js
@@ -1,26 +1,43 @@
 var locations = [
   {
     name: "ISS",
-    radius: 25,
+    radius: 15,
     fillKey: "ISS",
-    date: "1953-08-12",
-    latitude: 50.07,
-    longitude: 78.43,
+    date: "",
+    latitude: 0,
+    longitude: 0,
   },
 ];
 
-function getissloc() {
-  //   console.log("getissloc started");
+var pathnodes = [];
 
+function getissloc() {
+  map.bubbles([]);
   fetch("http://api.open-notify.org/iss-now.json")
     .then((response) => response.json())
     .then((data1) => {
-      console.log(data1);
+      // console.log(data1);
+
+      locations[0] = {
+        name: "ISS",
+        radius: 15,
+        fillKey: "ISS",
+        date: "",
+        latitude: 0,
+        longitude: 0,
+      };
+
       locations[0].latitude = data1.iss_position.latitude;
       locations[0].longitude = data1.iss_position.longitude;
       locations[0].date = data1.timestamp;
 
-      map.bubbles(locations, {
+      pathnodes.push(locations[0]);
+
+      // pathnodes[pathnodes.length - 1].fillKey = "path";
+      // pathnodes[pathnodes.length - 1].radius = 10;
+      // pathnodes[pathnodes.length - 1].name = "path";
+
+      map.bubbles(pathnodes, {
         popupTemplate: function (geo, data) {
           return [
             '<div class="hoverinfo">' + data.name,
@@ -32,6 +49,19 @@ function getissloc() {
           ].join("");
         },
       });
+
+      // map.bubbles(locations, {
+      //   popupTemplate: function (geo, data) {
+      //     return [
+      //       '<div class="hoverinfo">' + data.name,
+      //       "<br/>Date: " + data.date + "",
+      //       "<br/>latitude: " + data.latitude + "",
+      //       "<br/>longitude: " + data.longitude + "",
+
+      //       "</div>",
+      //     ].join("");
+      //   },
+      // });
     });
 }
 
@@ -39,17 +69,29 @@ var map = new Datamap({
   element: document.getElementById("mapbox"),
   responsive: true,
   fills: {
-    defaultFill: "#2a9d8f", //the keys in this object map to the "fillKey" of [data] or [bubbles]
+    defaultFill: "#2B454E", //the keys in this object map to the "fillKey" of [data] or [bubbles]
 
     ISS: "#000000",
+    path: "#ffffff",
+  },
+  geographyConfig: {
+    hideAntarctica: true,
+    borderWidth: 1,
+    borderOpacity: 1,
+
+    popupOnHover: true, //disable the popup while hovering
+    highlightOnHover: true,
+    highlightFillColor: "#576b72",
   },
 });
 
+getissloc();
 window.setInterval(function () {
-  map.bubbles([]);
   getissloc();
-}, 5000);
+}, 10000);
 
 window.addEventListener("resize", function () {
   map.resize();
 });
+
+// map.bubbles([]);
